5176d8c26f0dcf82cd7ca5f4f686da279b41c562
ELSON-341 <elson.sn314@gmail.com>
2024-04-03 20:15:10 -0300

    js

--- draggable slide/js/script.js
+++ draggable slide/js/script.js
@@ -8,3 +8,36 @@ const events = {
     mouse: {down: "mousedow", move: "mousemove", up: "mouseup" },
     touch: {down: "touchstart", move: "touchmove", up: "touchend"}
 }
+
+let deviceType = ''
+
+function isTouchDevice {
+    try {
+        document.createEvent('TouchEvent')
+        deviceType = 'touch'
+        return true
+    } catch(e) {
+        deviceType = 'mouse'
+        return false
+    }
+}
+
+function slideGenerator() {
+    images.forEach((image) => {
+        const slide = document.createElement('div')
+        slide.classList.add('slide')
+        slide.innerHTML = `< img src=${image} class="image">`
+        innerSlider.appendChild(slide)
+    })
+}
+
+innerSlider.style.gridTemplateColumns = `repeat(${images.length}, 1fr)`
+
+function handleDown(e) {
+    const active = true
+    const clienX = e.touches ? e.touches[0].clienX : e.clienX 
+    const containerRect = sliderContainer.getBoundingClientRect()
+    const sliderRect = innerSlider.getBoundingClientRect()
+    startX = clienX - (sliderRect.left - containerRect.left) 
+    sliderContainer.style.cursor = "grabbing"
+}
